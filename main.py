45efb4086401db981b3fd0eca6b17c8440fecbf2
Douglas Malan <125991889+MilanDouglas@users.noreply.github.com>
2024-03-26 09:05:18 +0000

    Practicing While-Loops
    Animals = ["lion", "giraffe", "gorilla", "parrots", "crocodile","deer", "swan"]
    i = 0
    New_animals = []
    while i < len(Animals):
        if len(Animals[i]) == 7:
            New_animals.append(Animals[i])
        i = i + 1
    print(New_animals)

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-import pyttsx3
-from PyPDF2 import PdfReader
-
-reader = PdfReader("book.pdf")
-
-reader = PdfReader("book.pdf")
-
-number_of_pages = len(reader.pages)
-page = reader.pages[0]
-text = page.extract_text()
-print(text)
-
-speaker = pyttsx3.init()
-speaker.save_to_file(text, 'book.mp3')
-speaker.runAndWait()
-
-speaker.stop()
+Animals = ["lion", "giraffe", "gorilla", "parrots", "crocodile","deer", "swan"]
+i = 0
+New_animals = []
+while i < len(Animals):
+    if len(Animals[i]) == 7:
+        New_animals.append(Animals[i])
+    i = i + 1
+print(New_animals)   
