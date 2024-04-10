94c5cff4b418bf88d709824e80e0821d07b33c35
Douglas Malan <125991889+MilanDouglas@users.noreply.github.com>
2024-04-10 07:36:21 +0000

    Using separator function to insert a separator.
    result = separator.join(fruits)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-input_list = [1,"hello", 1.618, "world"]
-result = ''.join(map(lambda x: str(x).replace('.', ''),input_list))
+#Join List with Custom Separator: Write a program that takes a list of strings and joins them into a single string using a custom separator. Allow the user to input the separator. For example, if the input list is ["apple", "orange", "banana"] and the separator is ";", the output should be "apple;orange;banana"
+fruits = ["apple", "orange", "banana"]
+separator = input("Enter the separator: ")
+result = separator.join(fruits)
 print(result)
